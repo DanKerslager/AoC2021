f400da8e33bdb3eba71079176fdf4515f229a890
DanKerslager <114998126+DanKerslager@users.noreply.github.com>
2022-12-18 21:20:12 +0100

    Add files via upload

---
+++ d2u1.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ADV;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author danke
+ */
+public class d2u1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long hor = 0;
+        long depth = 0;
+        int aim=0;
+        int num;
+        do  {
+            String dir = sc.next();
+            num = sc.nextInt();
+            switch (dir) {
+                case "forward":
+                    hor=hor+num;
+                    break;
+                case "up":
+                    depth=depth-num;
+                    break;
+                case "down":
+                    depth=depth+num;
+                    break;  
+            }
+        }while (num!=0);
+        System.out.println(depth*hor);
+        //System.out.println(aim);
+        
+        System.out.println(hor);
+    }
+    
+}
