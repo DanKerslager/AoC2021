d367af05fabca032b90d9020c8c373f584831e80
DanKerslager <114998126+DanKerslager@users.noreply.github.com>
2022-12-18 21:20:47 +0100

    Update AoC02_1.java

--- AoC02_1.java
+++ AoC02_1.java
@@ -1,20 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package ADV;
-
 import java.util.Scanner;
-
-/**
- *
- * @author danke
- */
 public class d2u1 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long hor = 0;
