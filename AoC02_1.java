8e4e2996904135529bb784d00c769675c2d7b6e2
DanKerslager <114998126+DanKerslager@users.noreply.github.com>
2022-12-18 21:19:24 +0100

    Create AoC02_1.java

---
+++ AoC02_1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class d2u1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long hor = 0;
+        long depth = 0;
+        int aim=0;
+        int num;
+        do  {
+            String dir = sc.next();
+            num = sc.nextInt();
+            switch (dir) {
+                case "forward":
+                    hor=hor+num;
+                    break;
+                case "up":
+                    depth=depth-num;
+                    break;
+                case "down":
+                    depth=depth+num;
+                    break;  
+            }
+        }while (num!=0);
+        System.out.println(depth*hor);
+    }
+    
+}
